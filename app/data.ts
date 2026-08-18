81f6fe7b058225fabd4e920bd0c4fc009bec8b78
Ahmed Messaad <78614946+RYANX9@users.noreply.github.com>
2026-08-18 11:22:54 +0100

    Update site content via admin dashboard

--- app/data.ts
+++ app/data.ts
@@ -100,7 +100,7 @@ export const projects: Project[] = [
     ],
     "link": "https://remp-8b15c.web.app/",
     "linkLabel": "Live",
-    "image": "/projects/remp-1787048466399.jpg"
+    "image": "/projects/remp-1787048533524.jpg"
   },
   {
     "id": "urtec",
