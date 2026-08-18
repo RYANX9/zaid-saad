772325241b4820ffa1bad3c2b98a9efcec31fec5
Ahmed Messaad <78614946+RYANX9@users.noreply.github.com>
2026-08-18 01:16:51 +0100

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -69,7 +69,7 @@ export const projects: Project[] = [
     tags: ["Flutter", "Firebase", "Google Cloud"],
     link: "https://remp-8b15c.web.app/",
     linkLabel: "Live",
-    image: "public/projects/project1.jpeg",
+    image: "projects/project1.jpeg",
   },
   {
     id: "urtec",
